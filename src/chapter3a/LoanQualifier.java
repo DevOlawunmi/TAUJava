afc51cfca07dfd8bd4832a9903d7e81b70b7cbb5
Ola <olawunmi263@yahoo.com>
2020-05-24 07:39:35 +0100

    Nested if statements pass

---
+++ src/chapter3a/LoanQualifier.java
@@ -0,0 +1,30 @@
+package chapter3a;
+/*
+To qualify for a loan, a person must make at least £30000
+and have been working on their job for 2 years
+ */
+
+import java.util.Scanner;
+
+public class LoanQualifier {
+    public static void main(String[] args) {
+// Get how much they earn
+        int requiredSalary = 30000;
+        int requiredService = 24;
+        System.out.println("Please enter current salary");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+        // Get how long they've been with the company for in months
+        System.out.println("Please enter in months how long you have been with the company");
+        int lengthOfService = scanner.nextInt();
+        scanner.close();
+
+        if (salary >= requiredSalary) {
+            if (lengthOfService >= requiredService) {
+                System.out.println("Congratulations, you qualify for a loan");
+            } else {
+                System.out.println("Unfortunately, you do not yet qualify for a loan due to LOS");
+            }
+        } else {System.out.println("Unfortunately you do not qualify, up your salary");}
+    }
+}
