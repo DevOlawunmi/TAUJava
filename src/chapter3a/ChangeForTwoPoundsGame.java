d25ab8d24ce43377a12c7b069c45fc307e5b3335
Ola <olawunmi263@yahoo.com>
2020-05-27 04:03:00 +0100

    Change for Two Pounds game

---
+++ src/chapter3a/ChangeForTwoPoundsGame.java
@@ -0,0 +1,36 @@
+package chapter3a;
+/*
+The objective of the game is to enter enough change to equal £2.
+Create a programme that asks the users to enter quantities of £1, 50p, 20p and 10p
+The programme should count up the total of all the change. If it is exactly £2, they win
+If it's more, tell them by how much they went over, if less tell them by how much they are short
+
+ */
+
+import java.util.Scanner;
+
+public class ChangeForTwoPoundsGame {
+    public static void main(String[] args) {
+
+        // Initialise what we know
+        //Get their input
+        int targetAmount = 200;
+        int exactFiftyP = 4;
+        int exactOnePound = 2;
+        int exactTwentyP = 10;
+        int exactTenP = 20;
+
+        System.out.println("How many one pound coins?");
+        Scanner scanner = new Scanner(System.in);
+        int onePoundQuantity = scanner.nextInt();
+        System.out.println("How many fifty p coins?");
+        int fiftyPQuantity = scanner.nextInt();
+        System.out.println("How many twenty p coins?");
+        int twentyPQuantity = scanner.nextInt();
+        if (onePoundQuantity == exactOnePound|| fiftyPQuantity==exactFiftyP||twentyPQuantity==exactTwentyP) {
+            System.out.println("You win");
+        }
+
+
+    }
+}
