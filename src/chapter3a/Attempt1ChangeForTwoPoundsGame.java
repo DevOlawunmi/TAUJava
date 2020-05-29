606341501780697d78169b8091491e62d69bf700
Ola <olawunmi263@yahoo.com>
2020-05-29 06:23:06 +0100

    Attempt 1 Change for Two Pounds game

--- src/chapter3a/Attempt1ChangeForTwoPoundsGame.java
+++ src/chapter3a/Attempt1ChangeForTwoPoundsGame.java
@@ -9,16 +9,16 @@ If it's more, tell them by how much they went over, if less tell them by how muc
 
 import java.util.Scanner;
 
-public class ChangeForTwoPoundsGame {
+public class Attempt1ChangeForTwoPoundsGame {
     public static void main(String[] args) {
 
         // Initialise what we know
-        //Get their input
-        int targetAmount = 200;
+        //Get user input
+        int targetAmount = 2;
         int exactFiftyP = 4;
         int exactOnePound = 2;
         int exactTwentyP = 10;
-        int exactTenP = 20;
+
 
         System.out.println("How many one pound coins?");
         Scanner scanner = new Scanner(System.in);
@@ -27,10 +27,12 @@ public class ChangeForTwoPoundsGame {
         int fiftyPQuantity = scanner.nextInt();
         System.out.println("How many twenty p coins?");
         int twentyPQuantity = scanner.nextInt();
-        if (onePoundQuantity == exactOnePound|| fiftyPQuantity==exactFiftyP||twentyPQuantity==exactTwentyP) {
-            System.out.println("You win");
+        if (onePoundQuantity == exactOnePound || fiftyPQuantity == exactFiftyP || twentyPQuantity == exactTwentyP) {
+            System.out.println("You have exactly £" + targetAmount + " Happy days!!!" );
+        } else {
+            System.out.println("You don't have exactly £" + targetAmount + " try again!");
+
+
         }
-
-
     }
 }
