e5a7b18bbfd1dbd2e3a4433324aeabab210eb81d
Ola <olawunmi263@yahoo.com>
2020-06-01 19:48:31 +0100

    CBF

---
+++ src/CodingBlackFemales1/Lesson1.java
@@ -0,0 +1,11 @@
+package CodingBlackFemales1;
+
+public class Lesson1 {
+
+    public static void main(String[] args) {
+        int myInt = 45;
+        float myFirstFloat = 3.45f;
+        String[] fruits = {"kiwi", "orange", "pear", "apple"};
+        System.out.println(fruits[0]);
+    }
+}
