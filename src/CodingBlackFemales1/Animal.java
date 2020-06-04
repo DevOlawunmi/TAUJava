c33386c91d49e35022b568acfa575f24cbe00c09
Ola <olawunmi263@yahoo.com>
2020-06-04 06:57:50 +0100

    Constructors Lesson

---
+++ src/CodingBlackFemales1/Animal.java
@@ -0,0 +1,26 @@
+package CodingBlackFemales1;
+
+
+
+public class Animal {
+
+    String food;
+    String drink;
+    public Animal(){}
+    public Animal(String food, String drink){
+this.food = food;
+this.drink = drink;
+    }
+    public String getFood(){
+        return food;
+    }
+    public void setFood(String food){
+        this.food = food;
+    }
+    public void eat(){
+        System.out.println("I am eating " + food);
+    }
+    public void drink(){
+        System.out.println("I am drinking water" + drink);
+    }
+}
---
+++ src/CodingBlackFemales1/Bear.java
@@ -0,0 +1,10 @@
+package CodingBlackFemales1;
+
+public class Bear {
+    public static void main(String[] args) {
+        Animal bear = new Animal();
+        
+    }
+
+
+}
