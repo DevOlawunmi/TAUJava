9269768f382ef510fa9151d115a16037a41b8a23
Ola <olawunmi263@yahoo.com>
2020-06-17 04:39:08 +0100

    Arrays Lessons

--- src/caleb/PracArrayList.java
+++ src/caleb/PracArrayList.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class PracArrayList {
     public static void main(String[] args) {
-        List<Integer> grades= Arrays.asList(6,32,31,7,9);
+        List<Integer> grades = Arrays.asList(6, 32, 31, 7, 9,91,78,87);
+        for (int i = 0; i < grades.size(); i++) {
+            System.out.println(grades.get(i));
+        }
     }
 }
